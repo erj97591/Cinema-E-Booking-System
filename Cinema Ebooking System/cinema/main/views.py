18156e1ce54adb2b9f981c02ef34be7ac80ca7c6
shadowbobx <garrettwill1220@gmail.com>
2022-03-18 20:50:34 -0400

    Hey This Do Be Working This Time?!
    django server that currently has one page but its more than nothing tbh

---
+++ Cinema Ebooking System/cinema/main/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index (response):
+	return HttpResponse("Hmm I can get this to work but when I copy and paste the html code from the github it thrwos 18 million erros man my spelling sucks")
