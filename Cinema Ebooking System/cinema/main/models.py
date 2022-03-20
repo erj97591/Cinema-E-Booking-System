e2b7e944c3a1a309898e32db459df5f92b292dee
shadowbobx <garrettwill1220@gmail.com>
2022-03-20 12:01:18 -0400

    Here We Go

--- Cinema Ebooking System/cinema/main/models.py
+++ Cinema Ebooking System/cinema/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+# if you make changes here do this to apply them:
+#python manage.py makemigrations main
+#python manage.py migrate
+#its like version control for git.
 # Create your models here.
+class custom_user(AbstractUser):
+    phone_number = models.IntegerField()
+    home_address = models.CharField(max_length=200)
+
+
--- Cinema Ebooking System/cinema/main/views.py
+++ Cinema Ebooking System/cinema/main/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 # Create your views here.
-def index (response):
-	return HttpResponse("<b>Hmm I can get this to work but when I copy and paste the html code from the github it thrwos 18 million erros man my spelling sucks</b>")
+def home(response, id):
+	if id == 2:
+		return render(response, "main/homepage2.html", {})
+	else: return render(response, "main/homepage1.html",{})
+def login_page(response):
+	if response.method=="POST":
+		email = response.POST['username']
+		password = response.POST['password']
+		
+	return render(response, "main/loginpage.html")
+def seat_view(response, id):
+	if id ==2:
+		return render(response, "main/seat.html", {})
+	else: return render(response, "main/seats2.html",{})
+def registration(response):
+	return render(response, "main/registrationpage.html")
