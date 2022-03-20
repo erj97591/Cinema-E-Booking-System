5788e116afb752fdf760d0271f6e7fe4974497f5
shadowbobx <garrettwill1220@gmail.com>
2022-03-19 20:22:01 -0400

    Just testing if basic Html like bolding works and it would appear to.
    Hmm

--- Cinema Ebooking System/cinema/main/views.py
+++ Cinema Ebooking System/cinema/main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index (response):
-	return HttpResponse("Hmm I can get this to work but when I copy and paste the html code from the github it thrwos 18 million erros man my spelling sucks")
+	return HttpResponse("<b>Hmm I can get this to work but when I copy and paste the html code from the github it thrwos 18 million erros man my spelling sucks</b>")
